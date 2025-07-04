6bdd974d8ee86567260dbbb4606986a2936a4afd
Nimely111 <samuelehnimely@gmail.com>
2025-07-04 00:03:26 +0000

    feat: customize local server port, and add prozy for feature backend fetching in the vite config file

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,17 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), tailwindcss()],
-    server:{
-      port: 3000,
-    }
+    server: {
+      port: 3000,             // Custom port
+      host: true,             // Expose to local network (0.0.0.0)
+      open: true,             // Open browser on dev start
+      https: false,           // Enable HTTPS (can be an object for custom certs)
+      proxy: {                // Proxy API requests to backend
+        '/api': {
+          target: 'http://localhost:8000',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '')
+        }
+      }
+},
 })
