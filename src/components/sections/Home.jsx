cbb41e3603e972453ccd50ab8525683b2a977da3
Nimely111 <samuelehnimely@gmail.com>
2025-07-08 01:30:07 +0000

    fix: resize the image

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -9,21 +9,25 @@ export const Home = () => {
     >
       <RevealOnScroll>
         <div className="text-center z-10 px-4">
-          <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent leading-right">
-            Hi, I'm Samuel E.H. Nimely
-          </h1>
-          {/* <img
-            src={Image}
-            alt="profile Image"
-            className="w-xs h-xs rounded-tl-full"
-          /> */}
-          <p className="tex-gray-400 text-lg mb-8 max-w-lg mx-auto">
-            I’m a full-stack developer who loves crafting clean, scalable web
-            applications. My goal is to build solutions that offer both
-            exceptional performance and a delightful user experience.
-          </p>
           <div className="flex justify-center space-x-4">
-            <a
+            <div>
+              <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent leading-right">
+                Hi, I'm Samuel E.H. Nimely
+              </h1>
+              <p className="tex-gray-400 text-lg mb-8 max-w-lg mx-auto">
+                I’m a full-stack developer who loves crafting clean, scalable
+                web applications. My goal is to build solutions that offer both
+                exceptional performance and a delightful user experience.
+              </p>
+            </div>
+            <img
+              src={Image}
+              alt="profile Image"
+              className="w-xs h-xs rounded"
+            />
+          </div>
+          <div className="flex justify-center space-x-4">
+            {/* <a
               href="#projects"
               className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.4)]"
             >
@@ -36,7 +40,7 @@ export const Home = () => {
              hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.2)] hover:bg-blue-500/10"
             >
               Contact Me
-            </a>
+            </a> */}
           </div>
         </div>
       </RevealOnScroll>
