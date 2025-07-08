e58da1ea40ef1d16221a560ca517118b9262bbd6
Nimely111 <samuelehnimely@gmail.com>
2025-07-08 01:03:35 +0000

    add image to home page

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -1,17 +1,22 @@
 import { RevealOnScroll } from "../RevealOnScroll";
+import Image from "../../../public/Profile.jpeg";
 
 export const Home = () => {
   return (
     <section
       id="home"
-      className="min-h-screen flex items-center justify-between relative"
+      className="min-h-screen flex items-center justify-center relative"
     >
       <RevealOnScroll>
         <div className="text-center z-10 px-4">
           <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent leading-right">
             Hi, I'm Samuel E.H. Nimely
           </h1>
-
+          {/* <img
+            src={Image}
+            alt="profile Image"
+            className="w-xs h-xs rounded-tl-full"
+          /> */}
           <p className="tex-gray-400 text-lg mb-8 max-w-lg mx-auto">
             I’m a full-stack developer who loves crafting clean, scalable web
             applications. My goal is to build solutions that offer both
