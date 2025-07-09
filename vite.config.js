82eea2326217591d75f0325520f883b139eff73e
Nimely111 <samuelehnimely@gmail.com>
2025-07-09 16:10:18 +0000

    refactor: to the original path

--- vite.config.js
+++ vite.config.js
@@ -5,19 +5,19 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss()],
-    base: process.env.VITE_BASE_PATH || "/sam-portfolio",
-    server: {
-      port: 3000,             // Custom port
-      // host: true,             // Expose to local network (0.0.0.0)
-      // open: true,             // Open browser on dev start
-      // https: false,           // Enable HTTPS (can be an object for custom certs)
-      // proxy: {                // Proxy API requests to backend
-      //   '/api': {
-      //     target: 'http://localhost:8000',
-      //     changeOrigin: true,
-      //     rewrite: (path) => path.replace(/^\/api/, '')
-      //   }
-      // }
-},
+  plugins: [react(), tailwindcss()],
+  base: process.env.VITE_BASE_PATH || "/my-portfolio",
+  server: {
+    port: 3000,             // Custom port
+    // host: true,             // Expose to local network (0.0.0.0)
+    // open: true,             // Open browser on dev start
+    // https: false,           // Enable HTTPS (can be an object for custom certs)
+    // proxy: {                // Proxy API requests to backend
+    //   '/api': {
+    //     target: 'http://localhost:8000',
+    //     changeOrigin: true,
+    //     rewrite: (path) => path.replace(/^\/api/, '')
+    //   }
+    // }
+  },
 })
