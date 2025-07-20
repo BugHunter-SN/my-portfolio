2058164a1f67acd5a53c41c7cf5220ecca016346
Nimely111 <samuelehnimely@gmail.com>
2025-07-20 22:22:08 +0000

    add a new sction for the printforge project and link

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -172,6 +172,46 @@ export const Projects = () => {
                 </a>
               </div>
             </div>
+            <div
+              className="
+              glass p-6 rounded-xl border border-white/10 
+              hover:-translate-y-1 hover:border-blue-500/30
+              hover:shadow-[0_4px_20px_rgba(59,130,246,0.1)]
+              transition-all
+            "
+            >
+              <h3 className="text-xl font-bold mb-2">
+                Printfordge A 3D Printing App for Designers
+              </h3>
+              <p className="text-gray-400 mb-4">
+                Scalable 3D platform for showcasing awesome 3D designs, with
+                real-time rendering and interactive features.
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["Socket.IO", "Express", "React", "Redis"].map((tech, key) => (
+                  <span
+                    key={key}
+                    className="
+                      bg-blue-500/10 text-blue-500 py-1 px-3 
+                      rounded-full text-sm
+                      transition
+                      hover:bg-blue-500/20 hover:-translate-y-0.5
+                      hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)]
+                    "
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+              <div className="flex justify-between items-center ">
+                <a
+                  href="https://print-forge.onrender.com/"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  View Project →
+                </a>
+              </div>
+            </div>
           </div>
         </div>
       </RevealOnScroll>
