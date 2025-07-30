61fafd264211d8ed3ca81b937617a4d3b3fad713
Nimely111 <samuelehnimely@gmail.com>
2025-07-30 22:41:36 +0000

    feat: and _blank attribute to projects links

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -13,7 +13,7 @@ export const Projects = () => {
             Featured Projects
           </h2>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-            <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+            {/* <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
               <h3 className="text-xl font-bold mb-2"> Cloud Platform</h3>
               <p className="text-gray-400 mb-4">
                 Scalable cloud infrastructure management with real-time
@@ -40,7 +40,7 @@ export const Projects = () => {
                   View Project →
                 </a>
               </div>
-            </div>
+            </div> */}
             <div
               className="
               glass p-6 rounded-xl border border-white/10 
@@ -165,6 +165,7 @@ export const Projects = () => {
               </div>
               <div className="flex justify-between items-center ">
                 <a
+                  target="_blank"
                   href="#"
                   className="text-blue-400 hover:text-blue-300 transition-colors my-4"
                 >
@@ -207,6 +208,7 @@ export const Projects = () => {
               </div>
               <div className="flex justify-between items-center ">
                 <a
+                  target="_blank"
                   href="https://print-forge.onrender.com/"
                   className="text-blue-400 hover:text-blue-300 transition-colors my-4"
                 >
