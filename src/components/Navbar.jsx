fa7553bd01357ce91a690df5b9a57e2c25df950e
Nimely111 <samuelehnimely@gmail.com>
2025-07-31 13:17:19 +0000

    update: removed the initials from the navbar and add img

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,21 @@
 import { useEffect } from "react";
+import Image from "../../public/Profile.jpeg";
 
 export const Navbar = ({ menuOpen, setMenuOpen }) => {
   useEffect(() => {
     document.body.style.overflow = menuOpen ? "hidden" : "";
   }, [menuOpen]);
+
   return (
     <nav className="fixed top-0 w-full z-40 bg-[rgba(10, 10, 10, 0.8)] backdrop-blur-lg border-b border-white/10 shadow-lg">
       <div className="max-w-5xl mx-auto px-4">
         <div className="flex justify-between items-center h-16">
-          <a href="#home" className="font-mono text-xl font-bold text-white">
-            {" "}
-            SE<span className="text-blue-500">HN</span>{" "}
+          <a href="#home" className="flex items-center">
+            <img
+              src={Image}
+              alt="Profile Image"
+              className="w-10 h-10 object-cover rounded-full shadow-lg hover:border-blue-400 transition-colors"
+            />
           </a>
 
           <div
@@ -25,29 +30,25 @@ export const Navbar = ({ menuOpen, setMenuOpen }) => {
               href="#home"
               className="text-gray-300 hover:text-white transition-colors"
             >
-              {" "}
               Home
             </a>
             <a
               href="#about"
               className="text-gray-300 hover:text-white transition-colors"
             >
-              {" "}
-              About{" "}
+              About
             </a>
             <a
               href="#projects"
               className="text-gray-300 hover:text-white transition-colors"
             >
-              {" "}
-              Projects{" "}
+              Projects
             </a>
             <a
               href="#contact"
               className="text-gray-300 hover:text-white transition-colors"
             >
-              {" "}
-              Contact{" "}
+              Contact
             </a>
           </div>
         </div>
